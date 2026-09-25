788a4d06811f917e6ab0079a601799b75d2fc0f9
sugumarmanoharan <sugumarmanokar1@gmail.com>
2026-09-25 19:33:45 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 704125-arithmetic-operators-GFG/704125-arithmetic-operators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        // code here
+        
+
+        System.out.printf("%d %d %d %.3f %d %d", p, q, r, s, t, u);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
