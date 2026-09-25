e471d068b34375053254c816f5d31a24c89f1e4e
sugumarmanoharan <sugumarmanokar1@gmail.com>
2026-09-25 10:29:12 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 878842-the-if-statement-GFG/878842-the-if-statement.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // code here
+         if(n>100)
+         {
+             System.out.println("Big");
+         }
+         
+         System.out.println("Number");
+         
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
