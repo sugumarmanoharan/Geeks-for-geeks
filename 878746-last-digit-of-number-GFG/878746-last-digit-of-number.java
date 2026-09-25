c30f42a3e3082733d18d0aa7238f459403cf51e5
sugumarmanoharan <sugumarmanokar1@gmail.com>
2026-09-25 19:34:46 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 878746-last-digit-of-number-GFG/878746-last-digit-of-number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+      
+            System.out.print(Math.abs(n%10));
+        }
+    }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
