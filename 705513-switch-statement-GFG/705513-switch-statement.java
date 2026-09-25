b4d8db38633cee1971bf000de1d3308e29ac70f1
sugumarmanoharan <sugumarmanokar1@gmail.com>
2026-09-25 19:34:14 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 705513-switch-statement-GFG/705513-switch-statement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
