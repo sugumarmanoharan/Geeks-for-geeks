d6fd2727df5814c445cbb897161b5ecb99291ca3
sugumarmanoharan <sugumarmanokar1@gmail.com>
2026-09-25 11:44:08 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 700796-learn-to-comment-GFG/700796-learn-to-comment.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // Prints a
+        System.out.println(a);
+
+        // Prints b
+        
+
+        // Prints c
+        System.out.println(c);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
