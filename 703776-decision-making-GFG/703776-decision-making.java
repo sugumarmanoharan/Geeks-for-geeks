4d297881630f716ccb25f5ee6b5e19ea629d0c1c
sugumarmanoharan <sugumarmanokar1@gmail.com>
2026-09-25 11:38:18 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 703776-decision-making-GFG/703776-decision-making.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // code here
+        if(n<m){
+            System.out.print("less");
+        
+        }
+        else if(n==m){
+            System.out.print("equal");
+            
+        }
+        else{
+            System.out.print("greater");
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
